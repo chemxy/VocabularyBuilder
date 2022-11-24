b624392aa08890c6c48855bf740db04a7dbe0b37
chemxy <chemxy110@gmail.com>
2022-11-24 10:43:49 -0800

    add styling

--- components/VocabularyDetailScreem.js
+++ components/VocabularyDetailScreem.js
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function VocabularyDetailScreen({ route }) {
     const { name, definition } = route.params;
     return (
         <View>
-            <Text>
+            <Text style={styles.nameBox}>
                 {name}
             </Text>
-            <Text>
+            <Text style={styles.definitionBox}>
                 {definition}
             </Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    nameBox: {
+        margin: 5,
+        borderWidth: 1,
+        borderRadius: 15,
+        padding: 5,
+    },
+    definitionBox: {
+        margin: 5,
+        borderWidth: 1,
+        borderRadius: 15,
+        padding: 5,
+    },
+})
